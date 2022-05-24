ee6b2a610f0ff71e5aba50747aeec7d2da88bd4d
bbayong28 <102298636+bbayong28@users.noreply.github.com>
2022-05-24 18:07:50 +0900

    Add files via upload

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -1,22 +1,14 @@
 $(function () {
 
-    $('.first_slider').slick({
+    $('.main_slider').slick({
         arrows: false,
         autoplay: true,
-        dots: true,
-    });
+        pauseOnHover: false,
+        pauseOnFocus: false
+
+
+    })
 
 
 
 })
-
-
-
-//$(function () {
-   // $('h1').hide()
-//}) function 으로 감싸줘야 위에 써도 실행됨.
-
-
-//$('h1').hide() <- 제이쿼리 메소드라고 부름!
-//slick 다운받으면 document.querySelector('h1').style.display = 'none'; 이 긴 문장을
-//$('h1').hide() 이렇게 간단히 쓰면 실행 됨.
---
+++ 슬라이드/js/first.js
@@ -0,0 +1,20 @@
+$(function () {
+
+    $('.first_slider').slick({
+        arrows: false,
+        autoplay: true,
+        dots: true,
+    });
+
+
+
+});
+
+//$(function () {
+   // $('h1').hide()
+//}) function 으로 감싸줘야 위에 써도 실행됨.
+
+
+//$('h1').hide() <- 제이쿼리 메소드라고 부름!
+//slick 다운받으면 document.querySelector('h1').style.display = 'none'; 이 긴 문장을
+//$('h1').hide() 이렇게 간단히 쓰면 실행 됨.
