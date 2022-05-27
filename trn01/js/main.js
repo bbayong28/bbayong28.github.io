70208aee810e84b964c551c97febfa2ff2b42ae5
bbayong28 <102298636+bbayong28@users.noreply.github.com>
2022-05-27 18:19:53 +0900

    Add files via upload

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -1,14 +1,16 @@
 $(function () {
 
+    $('.main_slider').on('init reInit afterChange', function () {
+        var here = $('.slick-current');
+        here.addClass('on').siblings().removeClass('on');
+    });
+
+
     $('.main_slider').slick({
         arrows: false,
         autoplay: true,
         pauseOnHover: false,
-        pauseOnFocus: false
-
-
-    })
-
-
-
+        pauseOnFocus: false,
+        fade: true,
+    });
 })
