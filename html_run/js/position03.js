327ea80fc216a3bf4af3938da1828c4297dde158
bbayong28 <102298636+bbayong28@users.noreply.github.com>
2022-06-03 17:23:40 +0900

    Add files via upload

--- html_run/js/position03.js
+++ html_run/js/position03.js
@@ -15,4 +15,14 @@ $(function () {
   });
  */
     
-})
+    $('.cover a').on('click', function (e) {
+        e.preventDefault();
+        let idx = $(this).parent().index();
+        console.log(100 * idx + "%", $('.sticky').height());
+        let h = $('.sticky').height();
+        $('.cover').removeClass('on');
+        $('html, body').animate({ scrollTop: idx * (h + 1) }, 1000)
+    })
+			
+
+}); 
