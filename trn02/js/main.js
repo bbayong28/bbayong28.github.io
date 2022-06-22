5cb1c54d0c21890988064c6ca08190f65cc6c1b0
bbayong28 <ghkdud5795@hanmail.net>
2022-06-22 16:35:30 +0900

    20220622

--- trn02/js/main.js
+++ trn02/js/main.js
@@ -38,4 +38,22 @@ $(function () {
     });
 
 
+    $('#utb').YTPlayer({
+        videoURL: 'https://youtu.be/dS9x5Ws67wI',
+        containment: '.utb',
+        autoPlay: true,
+        mute: true,
+        startAt: 0,
+        opacity: 1,
+        showControls: false,
+        playOnlyIfVisible:true,
+
+    });
+
+    $('.utb .zoom').on('click', function (e) {
+        e.preventDefault();
+        $('#utb').YTPFullscreen();
+    })
+
+
 })
