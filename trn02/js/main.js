9d45d52685556ed7e855fe54f9c275c801e17084
bbayong28 <ghkdud5795@hanmail.net>
2022-06-23 14:34:48 +0900

    20220623

--- trn02/js/main.js
+++ trn02/js/main.js
@@ -55,5 +55,60 @@ $(function () {
         $('#utb').YTPFullscreen();
     })
 
+    $('.productSlider').slick({
+        arrows:false,
+        //autoplay: true,
+        dots: true,
+        slidesToShow: 5,
+        centerMode:true,     
+    });
+
+    $('.product .productArrows i:nth-child(1)').on('click', function () {
+        $('.productSlider').slick('slickPrev')
+    })
+    $('.product .productArrows i:nth-child(2)').on('click', function () {
+        $('.productSlider').slick('slickNext')
+    })
+
+
+    $('.tab_menu li').on('click', function (event) {
+        event.preventDefault();
+        //console.log($(this), $(this).index(), event.target, event.currentTarget);
+        $('.tab_menu li').removeClass('on')
+        $(this).addClass('on');
+        var idx = $(this).index(); // 0, 1, 2
+        $('.tab_content>div').removeClass('on');
+        $('.tab_content>div').eq(idx).addClass('on');
+        //$('.tab_content>div').eq(idx).addClass('on').siblings().removeClass('on');
+    });
+
+
+    $('.tab_content .notice li').on('click', function (event) {
+        event.preventDefault();
+        var idx = $(this).index(); // 0, 1, 2
+        $('.right_content>li').removeClass('on');
+        $('.right_content>li').eq(idx).addClass('on');
+        console.log(ux)
+    });
+
+    $('.toTop').on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 200)
+    });
+
+
+    //$(window).on('scroll', function () {
+        //var sct = $(window).scrollTop();
+        //console.log(sct);
+    //}); 이까지하면 스크롤 숫자 console에 찍힘.
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+        if (sct > 500) {
+            $('.toTop').fadeIn(300);
+        } else {
+            $('.toTop').fadeOut(1000);
+        }
+    });
+
 
 })
