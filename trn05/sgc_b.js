1057b0fca8eb2e648da2d14d03753a3ca161466c
bbayong28 <ghkdud5795@hanmail.net>
2022-08-19 12:41:37 +0900

    20220819

--- trn05/sgc_b.js
+++ trn05/sgc_b.js
@@ -30,55 +30,28 @@ $(function () {
     var TXT = ['HOME', '01', '02', '03', '04', 'lee,s portfolio', 'copyright']
     //fullpage 
     $('.main').fullpage({
-        anchors: ['page01', 'page02', 'page03', 'page04', 'page05', 'footer'],
+        anchors: ['page01', 'page02', 'page03', 'page04', 'page05', 'page06', 'page07'],
         //navigation: true,
         css3: false,
         afterLoad: function (page, num) {
             setTimeout(function () {
                 $('.section').eq(num - 1).addClass('on').siblings().removeClass('on');
             });
-            $('.navBar li').eq(num - 1).addClass('on').siblings().removeClass('on');
+            $('.Gnb li').eq(num - 1).addClass('on').siblings().removeClass('on');
             $('.this_page').text(TXT[num - 1]);
-
-            if (num !== 1) { $('.Header').addClass('on') } else { $('.Header').removeClass('on') }
-            
-            num !== 1
-                ? $('.Header').addClass('on')
-                : $('.Header').removeClass('on');
-        
         },
     });
 
+    //https://github.com/aminta/fullPage.js-2.9.6 2버전 설명...
+    //https://desipossa.github.io/backup-01/%ED%92%80%ED%8E%98%EC%9D%B4%EC%A7%80%20%EC%8B%A4%EC%8A%B5%20f01/fullpage01.html
 
-    
-
-    //const BGC = $('.basicSlider').slick({
-    //    arrows: false,
-    //    asNavFor:'.basicSlider'
-    //});
-
-    $('.basicSlider').slick({
-        arrows: false,
-        autoplay: true,        
-        pauseOnHover:true,
-        asNavFor:'.basicSlider'
-    });
-
-
-    $('.basicSlider').on('afterChange', function (e, s, c) {
-        $('.productSlider .slide_menu li ').eq(c).addClass('on').siblings().removeClass('on');
-
-    });
-
-    $('.productSlider .slide_menu li ').on('click', function () { 
-        var idx = $(this).index();
-        $('.basicSlider').slick('slickGoTo', idx)
+    $('.slide').on('wheel', function (e) {
+        console.log(e.originalEvent.wheelDelta);
+        if (e.originalEvent.wheelDelta > 0 && $(this).index() > 0) {
+            $.fn.fullpage.moveSlideLeft();
+        }
+        if (e.originalEvent.wheelDelta < 0 && $(this).index() < 2) {
+            $.fn.fullpage.moveSlideRight()
+        }
     })
-
-
-
-
-
-
-
 })
