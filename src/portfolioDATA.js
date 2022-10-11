81bdf480382ac761f79506165f9818751354cefa
bbayong28 <ghkdud5795@hanmail.net>
2022-10-11 12:34:08 +0900

    20221011

---
+++ src/portfolioDATA.js
@@ -0,0 +1,8 @@
+const PD = [
+    {id: 1, title: "pf01", color:['#333', '#f00', '#369'], content:"first homepage",link:"bbayong28.github.io/Meta_quest2_tr", anchor:"pf01"},
+    {id: 2, title: "pf02", color:['#444', '#f0f', '#ff0'], content:"second homepage",link:"bbayong28.github.io/Jackson_tr", anchor:"pf02"},
+    {id: 3, title: "pf03", color:['#0ff', '#555', '#f5f5f5'], content:"third homepage",link:"bbayong28.github.io/massimo_dutti_tr", anchor:"pf03"},
+    {id: 4, title: "pf04", color:['#f2f2f2', '#ddd', '#369'], content:"fourth homepage",link:"bbayong28.github.io/massimo_dutti_tr", anchor:"pf04"},
+]
+
+export default PD;
