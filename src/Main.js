4f938c4ad3e370ce8f7544765e184e2d549d6440
bbayong28 <ghkdud5795@hanmail.net>
2022-10-21 17:40:20 +0900

    20221021

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,11 @@
 import React, { useEffect, useRef, useState } from 'react'
 import ReactFullpage from '@fullpage/react-fullpage';
 import './css/Main.scss';
-import { profile, portfolio, r_portfolio } from './Data';
+import { profile, portfolio, r_portfolio, cMenu } from './Data';
+import { BsGithub, BsEnvelope, BsTelephone } from "react-icons/bs";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 //portfolio배열에서 title을 빼서 배열을 만들어 쓰겠다.
 const AC = portfolio.map(it => it = it.title);
@@ -14,6 +18,7 @@ const Cover = ({ on, setOn }) => {
     const wheelStop = e => {
         e.stopPropagation();
     }
+    //cover메뉴 열었을 때 wheel 막기  
     useEffect(() => {
         cover.current.addEventListener('wheel', wheelStop);
         return () => {
@@ -82,11 +87,12 @@ const Main_copy = () => {
                     return (
                         <ReactFullpage.Wrapper>
                             <div className="section main">
-                               
                                 <div className='case'>
                                     <div className="inner">
-                                        <div className='circle'></div>
-                                        <h1>Hello,<br/>I'm Hwayeong</h1>
+                                        <p className='tMenu'>HOME</p>
+                                        {/* <p>{cMenu.title}</p> */}
+                                        
+                                        <h1>Hello, I'm<br/>Hwayeong Shin.</h1>
                                         <p>{profile.title}</p>
                                     </div>
                                     
@@ -105,7 +111,7 @@ const Main_copy = () => {
                                 portfolio.map((it, idx) => {
                                     return (
                                         <div className="section pf_main" >
-                                            <div className="Wrapper bgAnimation">
+                                            {/* <div className="Wrapper bgAnimation">
                                                 <div className="innerWrapper bgTop">
                                                     <div className='wave top'></div>
                                                 </div>
@@ -115,7 +121,7 @@ const Main_copy = () => {
                                                 <div className="innerWrapper bgBottom">
                                                     <div className='wave bottom'></div>
                                                 </div>
-                                            </div>
+                                            </div> */}
                                             <div className='case'>
                                                 <div className="left">
                                                     <div className="top">
@@ -179,17 +185,6 @@ const Main_copy = () => {
                                 r_portfolio.map((it, idx) => {
                                     return (
                                         <div className="section pf_main" >
-                                            <div className="Wrapper bgAnimation">
-                                                <div className="innerWrapper bgTop">
-                                                    <div className='wave rTop'></div>
-                                                </div>
-                                                <div className="innerWrapper bgMiddle">
-                                                    <div className='wave rMiddle'></div>
-                                                </div>
-                                                <div className="innerWrapper bgBottom">
-                                                    <div className='wave rBottom'></div>
-                                                </div>
-                                            </div>
                                             <div className='case'>
                                                 <div className="left">
                                                     <div className="top">
@@ -249,18 +244,55 @@ const Main_copy = () => {
                                     )
                                 })
                             }
-                            <div className="section profile">
-                                <div className='case'>
-                                    <div className="w1700">
-                                        <div className='left'>                                            
-                                            <p>HAVE YOU ENJOY!!</p>
-                                            {/* <p>즐거우셨기를 바랍니다.</p> */}
-                                            <span>{profile.email}</span>
+                            <div className="section contact">
+                            <div className='oon'>
+                                <div className='up'>
+                                    <div className='left'>                                            
+                                        <h2>MY<br/>CONTACT</h2>   
+                                    </div>
+                                    <div className='right'>
+                                        <form>       
+                                            <div className="top">
+                                                <div className="input_box"> 
+                                                    <input type="text" id="name" placeholder='Name'/>
+                                                </div>
+                                                <div className="input_box">       
+                                                    <input type="text" id="email" placeholder='Email'/>
+                                                </div>
+                                            </div>
+                                            <div className="input_box">
+                                                <textarea name="comment" placeholder='Comment' id="comment" cols="100" rows="20"></textarea>
+                                            </div>
+                                            <div className="input_submit">                            
+                                                <input type="submit" value="SUBMIT" />
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>                
+                                <div className='down'>
+                                    <div className='top'>
+                                        <div className='left'>
+                                            SHIN HWAYEONG
                                         </div>
-                                        <div className='right'></div>
+                                    <div className='right'>
+                                            <a href='#!'>
+                                                <BsGithub />
+                                            </a>
+                                            <a href='mailto:ghkdud5795@naver.com'>
+                                                <BsEnvelope />
+                                            </a>
+                                            <a href='tel:010-7777-8888'>
+                                                <BsTelephone />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div className='bottom'>
+                                        THANK YOU FOR VIEWING MY PORTFOLIO.
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                           {/*  <Contact /> */}
                         </ReactFullpage.Wrapper>
                     );
                 }}
